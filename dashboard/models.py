83169c9206426f04b3fe009bb8ac62286b385d30
Karmveer <pranshtechnewjoinee@gmail.com>
2025-08-11 14:51:06 +0530

    updated model

--- dashboard/models.py
+++ dashboard/models.py
@@ -534,7 +534,7 @@ class BankTransferPayment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bank_transfer_payments")
     name = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    paid = models.BooleanField(default=False)
+    paid = models.BooleanField(default=False)   
     verified_by_admin = models.BooleanField(default=False)
     admin_notes = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
