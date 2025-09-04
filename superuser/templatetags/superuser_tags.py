c015e1ceed3cdb5508098c3fcac2ed17366644e2
Kirtan <kirtan.pranshtech@gmail.com>
2025-09-04 11:30:36 +0530

    Staff Permission Changes .

--- superuser/templatetags/superuser_tags.py
+++ superuser/templatetags/superuser_tags.py
@@ -50,3 +50,29 @@ def tag_user_has_permission(user, perm_codename):
         # print('user permission -----------', user.has_perm(perm_codename))
         return user.has_perm(perm_codename)
     return False
+
+@register.filter
+def tag_format_permission_name(value):
+    if value.lower().startswith('can '):
+        value = value[4:]
+
+    value = value.replace("change user", "Update User")
+
+    # Handle RatingReview → Rating/Review
+    value = value.replace("rating review", "Rating/Review")
+
+    # Handle RFQRequest → RFQ Request (if needed)
+    value = value.replace("Rfqrequest", "RFQ request")
+    value = value.replace("Rfq Request", "RFQ request")
+
+
+    return value.title()
+
+
+@register.filter
+def tag_format_model_name(model_name):
+    format_model_name = model_name.title()
+    if model_name == 'ratingreview': format_model_name = "Rating/Review"
+    elif model_name == 'rfqrequest': format_model_name = "RFQ request"
+
+    return format_model_name
